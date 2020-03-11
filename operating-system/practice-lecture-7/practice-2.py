034f84df956cf8cc6a12c1ee03fec9a520ca4e01
ZhaohengLi <zhaohengli.thu@gmail.com>
2020-03-11 14:11:45 +0800

    添加了测试的输入输出

--- operating-system/practice-lecture-7/practice-2.py
+++ operating-system/practice-lecture-7/practice-2.py
@@ -1,3 +1,4 @@
+# 测试输入
 input = ['c', 'c', 'd', 'b', 'c', 'e', 'c', 'e', 'a', 'd']
 T = 2
 
@@ -30,3 +31,26 @@ for i in range(0, len(input)):
         lastPF = i+1
     print ""
     print pages_lastHit
+
+# 页面调度过程如下
+# Initial State: {'a': 0, 'e': 0, 'd': 0}
+# Visit page c (Interrupt)page Fault
+# {'a': 0, 'c': 1, 'e': 0, 'd': 0}
+# Visit page c Hit c
+# {'a': 0, 'c': 2, 'e': 0, 'd': 0}
+# Visit page d Hit d
+# {'a': 0, 'c': 2, 'e': 0, 'd': 3}
+# Visit page b (Interrupt)page Fault
+# {'c': 2, 'b': 4, 'd': 3}
+# Visit page c Hit c
+# {'c': 5, 'b': 4, 'd': 3}
+# Visit page e (Interrupt)page Fault
+# {'c': 5, 'b': 4, 'e': 6, 'd': 3}
+# Visit page c Hit c
+# {'c': 7, 'b': 4, 'e': 6, 'd': 3}
+# Visit page e Hit e
+# {'c': 7, 'b': 4, 'e': 8, 'd': 3}
+# Visit page a (Interrupt)page Fault
+# {'a': 9, 'c': 7, 'e': 8}
+# Visit page d (Interrupt)page Fault
+# {'a': 9, 'c': 7, 'e': 8, 'd': 10}
